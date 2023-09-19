a3f837d6472bd5149e5476e8586f90270be9e4ea
George Castillo <gmcastil@gmail.com>
2023-09-19 17:02:18 -0600

    Initial cut at automated Debian derived system for ARM
    - Buildroot works, but it isn't really what I need or want for a debug
    and development platform.
    - Debian 12 has a lot more functionality, which is something I'm going
    to want as my needs / desires want (e.g., I want to be able to compile
    natively and that's really beyond the scope of what the buildroot folks
    are trying to do).
    - This requires end users to have a QEMU emulation setup that supports
    transparent emulation. Note that there may also be problems when trying
    to engage things like SSL from the `chroot` environment, so be aware on
    what is happening.

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Directories
 export UZED_SBC_EXTERN_DIR="../extern"
 export UZED_SBC_BUILD_DIR="../build"
+
+# Constants for building a Debian root filesystem
+export DEBIAN_ARCH="armhf"
+export DEBIAN_SUITE="bookworm"
+export DEBIAN_MIRROR="http://deb.debian.org/debian"
+export DEBIAN_INCLUDE=
+
+# QEMU related constants
+export QEMU_ARCH="arm"
 
