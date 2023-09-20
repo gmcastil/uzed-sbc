c55286fc56abb7eb006649b8c3492cad2effbbfb
George Castillo <gmcastil@gmail.com>
2023-09-20 16:17:21 -0600

    Can now build a Debian derived root filesystem for the ARM board
    - Plan is to create an SD card image that can be used to boot the board
    - Next step is to automate creation of the /boot partition, put the
    kernel, bootloaders, and device tree on the partition
    - Need to suppress all the output in the second stage build script and
    constrain it to the build script
    - On device trees, I'm curious as to whether U-boot even needs my device
    tree at compile time - the FSBL contains enough information to allow the
    bootloader to boot.  Something to think about.
    
    Ultimately what I want automated is enough to get a system up and
    running with my own FSBL, kernel, U-Boot, and root filesystem, along
    with a device tree of some sort that provides access to whatever is in
    the PL that is expressed by the exported hardware design.

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -8,8 +8,22 @@ export UZED_SBC_BUILD_DIR="../build"
 export DEBIAN_ARCH="armhf"
 export DEBIAN_SUITE="bookworm"
 export DEBIAN_MIRROR="http://deb.debian.org/debian"
-export DEBIAN_INCLUDE=
+
+# There are a ton of packages that either do not get pulled in by default and
+# need to be or are extremely useful and it is far quicker to do it now rather
+# than from an SD card
+DEBIAN_INCLUDE="sudo,device-tree-compiler,i2c-tools,build-essential,screen"
+DEBIAN_INCLUDE+=",pkg-config,libglib2.0-dev,texinfo,dh-autoreconf"
+DEBIAN_INCLUDE+=",autotools-dev,git,debhelper,fakeroot,devscripts,bc"
+DEBIAN_INCLUDE+=",libi2c-dev,cloud-guest-utils,dbus,perl-openssl-defaults"
+DEBIAN_INCLUDE+=",vim,rsync,haveged"
+export DEBIAN_INCLUDE
 
 # QEMU related constants
 export QEMU_ARCH="arm"
 
+# Second stage related constants
+export DEBIAN_USER="debian"
+export DEBIAN_PASS="none"
+export DEBIAN_HOSTNAME="uzed-sbc"
+export DEBIAN_SERIAL="ttyPS0"
