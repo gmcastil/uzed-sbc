a82ebc0cf4b6e08fd98b8387bf359491d4993107
George Castillo <gmcastil@gmail.com>
2023-10-02 07:15:25 -0600

    Modified basic module to register as a platform device
    - Using _probe() and _remove() functions instead of module_init()
    and module_exit()
    - Not sure how the registration works, when it gets called, etc. and for
    reasons I do not understand yet, dmesg does not pick up my functions
    yet.

--- src/drivers/basic/basic.c
+++ src/drivers/basic/basic.c
@@ -1,23 +1,76 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
 #include <linux/printk.h>
 
-int __init basic_init(void)
+#define BASIC_MODNAME		"basic"
+
+static int basic_probe(struct platform_device *pdev)
 {
 	pr_info("Basic module init\n");
 	return 0;
 }
 
-void __exit basic_exit(void)
+static int basic_remove(struct platform_device *pdev)
 {
 	pr_info("Basic module exit\n");
-	return;
+	return 0;
 }
 
-module_init(basic_init);
-module_exit(basic_exit);
+#ifdef CONFIG_OF
+static const struct of_device_id basic_of_match[] = {
+	{ .compatible = BASIC_MODNAME, },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, basic_of_match);
+#endif
+
+/* Create a platform driver for this device */
+static struct platform_driver basic_platform_driver = {
+	/* int (*probe)(struct platform_device *); */
+	.probe		= basic_probe,
+	/*
+	 * Traditionally the remove callback returned an int which however is
+	 * ignored by the driver core. This led to wrong expectations by driver
+	 * authors who thought returning an error code was a valid error
+	 * handling strategy. To convert to a callback returning void, new
+	 * drivers should implement .remove_new() until the conversion it done
+	 * that eventually makes .remove() return void.
+	 *
+	 *   int (*remove)(struct platform_device *);
+	 *   void (*remove_new)(struct platform_device *);
+	 */
+	.remove		= basic_remove,
+	.remove_new	= NULL,
+	/*
+	 * Power management which I will ignore for this device
+	 *   void (*shutdown)(struct platform_device *);
+	 *   int (*suspend)(struct platform_device *, pm_message_t state);
+	 *   int (*resume)(struct platform_device *);
+	 */
+	.shutdown	= NULL,
+	.suspend	= NULL,
+	.resume		= NULL,
+
+	/* struct device_driver driver; */
+	.driver = {
+		.name		= BASIC_MODNAME,
+		.of_match_table = of_match_ptr(basic_of_match),
+	},
+	/* const struct platform_device_id *id_table; */
+	.id_table	= NULL,
+	/* bool prevent_deferred_probe; */
+	.prevent_deferred_probe	= false,
+
+	/* bool driver_managed_dma; */
+	.driver_managed_dma	= false	
+};
+
+/* Register this module as a platform device */
+module_platform_driver(basic_platform_driver);
 
 MODULE_AUTHOR("George Castillo");
 MODULE_DESCRIPTION("Demonstrate module build, insertion and removal functionality");
-MODULE_LICENSE("MIT");
+MODULE_LICENSE("GPL");
 
