29cc8feabdb991008970e15db1067f65e5729ce2
George Castillo <gmcastil@gmail.com>
2023-10-10 21:32:29 +0000

    Working bootscript that can be compiled on the board.

--- src/boot/boot.cmd
+++ src/boot/boot.cmd
@@ -1,19 +1,15 @@
 #!/bin/sh
 
-# shellcheck disable=SC2121
-# Turn on debugging
-set xtrace yes
-
 # Note that U-boot and the kernel do not share the same device enumeration, so
 # the root device and partitions numbers are not expected to be the same in
 # general
-default_bootargs="earlycon console=ttyPS0,111520 root=/dev/mmcblk0p2 rw rootwait earlyprintk"
+default_bootargs="earlycon console=ttyPS0,111520 clk_ignore_unused uio_pdrv_genirq.of_id=generic-uio root=/dev/mmcblk0p2 rw rootwait earlyprintk"
 
-bitstream="system.bit"
-bitstream_size="0x001fcc08"
+bitstream="uzed_sbc_top.bit.bin"
+bitstream_size="0x001fcba0"
 
 uimage="uImage"
-fdt="system-devel.dtb"
+fdt="system.dtb"
 
 boot_intf="mmc"
 boot_dev=0
@@ -29,10 +25,9 @@ kernel_load_addr="0x02000000"
 fdt_load_addr="0x01f00000"
 
 # Copy the bitstream to the same location the kernel will eventually go
-if test -e "${boot_intf}" "${boot_dev}:${boot_part}" "${bitstream}"; then
+if test -e "${boot_intf}" "${boot_dev}":"${boot_part}" "${bitstream}"; then
     echo "Loading bitstream to ${kernel_load_addr}"
-    fatload "${boot_intf}" "${boot_dev}:${boot_part}" \
-        "${kernel_load_addr}" "${bitstream}"
+    fatload "${boot_intf}" "${boot_dev}":"${boot_part}" "${kernel_load_addr}" "${bitstream}"
     # Load the FPGA image to device 0 (presumably there are PL configurations
     # where this is non-zero)
     fpga load 0 "${kernel_load_addr}" "${bitstream_size}"
@@ -53,8 +48,7 @@ fi
 # Load the kernel to the same location as the FPGA image was
 if test -e "${boot_intf}" "${boot_dev}":"${boot_part}" "${uimage}"; then
     echo "Loading ${uimage} to ${kernel_load_addr}"
-    fatload "${boot_intf}" "${boot_dev}":"${boot_part}" \
-        "${kernel_load_addr}" "${uimage}"
+    fatload "${boot_intf}" "${boot_dev}":"${boot_part}" "${kernel_load_addr}" "${uimage}"
 else
     echo "ERROR: Could not find ${uimage}"
     safe_to_boot=1
@@ -63,15 +57,14 @@ fi
 # Load the FDT blob to the desired location
 if test -e "${boot_intf}" "${boot_dev}":"${boot_part}" "${fdt}"; then
     echo "Loading flattened device tree ${fdt} to ${fdt_load_addr}"
-    fatload "${boot_intf}" "${boot_dev}":"${boot_part}" \
-        "${fdt_load_addr}" "${fdt}"
+    fatload "${boot_intf}" "${boot_dev}":"${boot_part}" "${fdt_load_addr}" "${fdt}"
 else
     echo "ERROR: Could not find flattened device tree ${fdt}"
     safe_to_boot=1
 fi
 
 # If kernel and FDT were found and loaded we can boot
-if test "${safe_to_boot}" -eq 1; then
+if test "${safe_to_boot}" -eq 0; then
     echo "Booting application from memory"
     bootm "${kernel_load_addr}" "-" "${fdt_load_addr}"
 fi
