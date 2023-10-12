a4a0e5d7c0b9676e2279b58399fce835d47a4969
George Castillo <gmcastil@gmail.com>
2023-10-12 13:02:06 +0000

    First block RAM read / write operations from userspace are successful!

--- src/tools/smoke_test.c
+++ src/tools/smoke_test.c
@@ -3,14 +3,38 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 #include <sys/mman.h>
+
+void read_bram(void *mem)
+{
+	uint32_t rd_data;
+	rd_data = * (uint32_t *) mem;
+	printf("0x%08"PRIx32"\n", rd_data);
+	return;
+}
+
+int write_bram(void *mem, uint32_t wr_data)
+{
+	uint32_t rd_data;
+	* (uint32_t *) mem = wr_data;
+	rd_data = * (uint32_t *) mem;
+	printf("Wrote 0x%08"PRIx32"\tRead 0x%08"PRIx32"\n", wr_data, rd_data);
+	if (rd_data != wr_data) {
+		return -1;
+	} else {
+		return 0;
+	}
+}
 
 int main(int argc, char *argv[])
 {
 	int fd;
 	int ret;
-	
+	int save_errno;
+
 	void *mem = NULL;
 	/* 
 	 * This will normally be either a subset of the memory in the PL or the
@@ -19,13 +43,34 @@ int main(int argc, char *argv[])
 	 */
 	size_t mem_size = 0x8000;
 
-	fd = open("/dev/uio1", O_RDWR | O_SYNC);
+	fd = open("/dev/uio0", O_RDWR | O_SYNC);
 	if (fd < 0) {
 		printf("Could not open /dev/uio0: %s\n", strerror(errno));
 		return -1;
 	}
-	mem = mmap(NULL, mem_size, PROT_READ | PROT_WRITE, fd, 0);
+
+	/* Map our UIO device into virtual memory for reading and writing */
+	mem = mmap(NULL, mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	save_errno = errno;
+	close(fd);
+	if (!mem) {
+		printf("Could not map uio device: %s\n", strerror(save_errno));
+		return -1;
+	}
+
+	read_bram(mem);
+	ret = write_bram(mem, 0xdead0000);
+	if (!ret) {
+		printf("Success\n");
+	}
+
+	/* Finished with the device, so we can unmap the memory prior to exit */
 	ret = munmap(mem, mem_size);
+	if (ret) {
+		printf("Could not unmap region: %s\n", strerror(errno));
+		return -1;
+	}
+
 
 	close(fd);
 	return 0;
