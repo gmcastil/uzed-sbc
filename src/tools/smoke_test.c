11377ba6d4b6cad62979afc7d58674f73e696ca0
George Castillo <gmcastil@gmail.com>
2023-10-12 06:11:54 -0600

    Trying to map / unmap `uio` devices

--- src/tools/smoke_test.c
+++ src/tools/smoke_test.c
@@ -9,14 +9,23 @@
 int main(int argc, char *argv[])
 {
 	int fd;
+	int ret;
 	
 	void *mem = NULL;
+	/* 
+	 * This will normally be either a subset of the memory in the PL or the
+	 * entire range. It can and should be based upon an entry that is
+	 * in /sys/devices/..../map
+	 */
+	size_t mem_size = 0x8000;
 
 	fd = open("/dev/uio1", O_RDWR | O_SYNC);
 	if (fd < 0) {
 		printf("Could not open /dev/uio0: %s\n", strerror(errno));
 		return -1;
 	}
+	mem = mmap(NULL, mem_size, PROT_READ | PROT_WRITE, fd, 0);
+	ret = munmap(mem, mem_size);
 
 	close(fd);
 	return 0;
