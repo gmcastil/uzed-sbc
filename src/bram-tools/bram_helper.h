f4943729fe5c15d44d297cdb57e8562d5075b265
George Castillo <gmcastil@gmail.com>
2023-11-01 12:54:50 +0000

    Adding include bands

--- src/bram-tools/bram_helper.h
+++ src/bram-tools/bram_helper.h
@@ -1,3 +1,6 @@
+#ifndef BRAM_HELPER_H
+#define BRAM_HELPER_H
+
 #include "bram_resource.h"
 
 void print_bram_init_error(int uio_number, int map_number);
@@ -6,3 +9,4 @@ int bram_set_map_info(struct bram_resource *bram);
 int bram_map_resource(struct bram_resource *bram);
 int bram_unmap_resource(struct bram_resource *bram);
 
+#endif /* BRAM_HELPER_H */
