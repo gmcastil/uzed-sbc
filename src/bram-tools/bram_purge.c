b9a6bf17d3c31c0f1de2c4dc8ab9e5f413e854fb
George Castillo <gmcastil@gmail.com>
2023-11-06 19:13:22 +0000

    Purge function grew to be a lot more than a simple function
    - Supports checkerboard, incrementing, and value patterns
    - Can select a subset, starting address only, or allow the default to
      just be whatever the entire size of the block RAM is
    - Works in hardware, but hasn't been extensively tested

--- src/bram-tools/bram_purge.c
+++ src/bram-tools/bram_purge.c
@@ -12,8 +12,92 @@
 #include "bram_resource.h"
 #include "bram_helper.h"
 
+/* Checkerboard patterns begin with this and complement it each write */
+#define XBOARD_START		0x55
+
+int purge_bram_by_value(struct bram_resource *bram, uint16_t start_addr,
+		uint16_t stop_addr, uint8_t purge_val)
+{
+	uint8_t *map_pos = NULL;
+	uint16_t num_to_write = 0;
+	uint16_t num_written = 0;
+
+	map_pos = bram->map;
+	if (!map_pos) {
+		fprintf(stderr, "Error: NULL memory map\n");
+		return -1;
+	}
+	printf("Purging 0x%04"PRIx16" to 0x%04"PRIx16" with value 0x%02"PRIx8"\n",
+			start_addr, stop_addr, purge_val);
+	num_to_write = 1 + (stop_addr - start_addr);
+	for (int i = 0; i < num_to_write; i++) {
+		*map_pos = purge_val;
+		map_pos++;
+		num_written++;
+	}
+	return (num_to_write - num_written);
+}
+
+int purge_bram_by_xboard(struct bram_resource *bram, uint16_t start_addr,
+		uint16_t stop_addr)
+{
+	uint8_t purge_val = XBOARD_START;
+	uint8_t *map_pos = NULL;
+	uint16_t num_to_write = 0;
+	uint16_t num_written = 0;
+
+	map_pos = bram->map;
+	if (!map_pos) {
+		fprintf(stderr, "Error: NULL memory map\n");
+		return -1;
+	}
+	printf("Purging 0x%04"PRIx16" to 0x%04"PRIx16" with checkerboard pattern\n",
+			start_addr, stop_addr);
+	num_to_write = 1 + (stop_addr - start_addr);
+	for (int i = 0; i < num_to_write; i++) {
+		*map_pos = purge_val;
+		map_pos++;
+		purge_val = ~purge_val;
+		num_written++;
+	}
+	return (num_to_write - num_written);
+}
+
+int purge_bram_by_incr(struct bram_resource *bram, uint16_t start_addr,
+		uint16_t stop_addr)
+{
+	/*
+	 * We are going to pull the 8-bits from the address we start with and
+	 * then increment from there, relying on the fact that unsigned overflow
+	 * is defined so that the pattern we see in hardware wraps around as
+	 * expected.
+	 */
+	uint8_t purge_val;
+	uint8_t *map_pos = NULL;
+	uint16_t num_to_write = 0;
+	uint16_t num_written = 0;
+
+	map_pos = bram->map;
+	if (!map_pos) {
+		fprintf(stderr, "Error: NULL memory map\n");
+		return -1;
+	}
+	printf("Purging 0x%04"PRIx16" to 0x%04"PRIx16" with incrementing pattern\n",
+			start_addr, stop_addr);
+
+	num_to_write = 1 + (stop_addr - start_addr);
+	purge_val = (start_addr & 0x00ffU) >> 8;
+	for (int i = 0; i < num_to_write; i++) {
+		*map_pos = purge_val;
+		map_pos++;
+		purge_val++;
+		num_written++;
+	}
+	return (num_to_write - num_written);
+}
+
 /*
-int bram_purge(struct bram_resource *bram, size_t start_addr,
+int purge_by_val(struct bram_resource *bram, size_t start_addr,
 		size_t stop_addr, uint8_t val)
 {
 	uint8_t *map_pos = NULL;
@@ -56,9 +140,18 @@ int bram_purge(struct bram_resource *bram, size_t start_addr,
 	return (int) num_written;
 }
 */
+
 void print_usage()
 {
-	printf("Usage: bram_purge DEVICE MAP VALUE [START [END]]\n");
+	printf("Usage: bram_purge [-x] [-i] [-v VALUE] DEVICE MAP [START [END]]\n");
+	printf("\n");
+	printf("Options:\n");
+	printf("  %-15s%-30s\n", "-h", "display program usage");
+	printf("  %-15s%-30s\n", "-x", "purge with checkerboard pattern");
+	printf("  %-15s%-30s\n", "-i", "purge with incrementing pattern");
+	printf("  %-15s%-30s\n", "-v VALUE", "purge with value");
+	printf("\n");
+
 	return;
 }
 
@@ -68,81 +161,151 @@ int main(int argc, char *argv[])
 	int map_number;
 	uint8_t purge_val;
 	uint16_t start_addr;
-	uint16_t end_addr;
+	uint16_t stop_addr;
 
 	bool start_given;
-	bool end_given;
+	bool stop_given;
+
+	bool by_incr = false;
+	bool by_xboard = false;
+	bool by_value = false;
+
+	int result;
+	int retval;
+	struct bram_resource bram;
+	int num_pos_args;
 
 	int opt;
-	while ((opt = getopt(argc, argv, "")) != -1) {
+	while ((opt = getopt(argc, argv, "hixv:")) != -1) {
 		switch (opt) {
+			case 'h':
+				print_usage();
+				return 0;
+			case 'i':
+				by_incr = true;
+				break;
+			case 'x':
+				by_xboard = true;
+				break;
+			case 'v':
+				by_value = true;
+				if (str_to_uint8_t(&purge_val, optarg)) {
+					fprintf(stderr, "Error: Bad purge value\n");
+					return 1;
+				}
+				break;
+			case '?':
+				if (optopt == 'v') {
+					fprintf(stderr, "Error: No purge value specified\n");
+				} else if (isprint(optopt)) {
+					fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+				} else {
+					fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+				}
+				return 1;
 			default:
 				print_usage();
 				return 1;
 			}
 	}
 
-	if ((argc - optind) < 3) {
+	/* Require at least 2 and at most 4 positional arguments */
+	num_pos_args = argc - optind;
+	if ((num_pos_args < 2) || (num_pos_args > 4)) {
+		fprintf(stderr, "Error: Incorrect number of positional arguments\n");
 		print_usage();
 		return 1;
+	}
+	/* TODO Sanitize these two values */
+	uio_number = atoi(argv[optind]);
+	map_number = atoi(argv[optind + 1]);
+
+	/* Process the remaining 0 to 2 positional arguments */
+	switch (num_pos_args) {
+		/* No start or end address given */
+		case 2:
+			start_given = false;
+			stop_given = false;
+			break;
+		/* Only given starting address */
+		case 3:
+			if (str_to_uint16_t(&start_addr, argv[optind + 2])) {
+				fprintf(stderr, "Error: Bad starting address\n");
+				return -1;
+			}
+			start_given = true;
+			stop_given = false;
+			break;
+		/* Start and end addresses given */
+		case 4:
+			if (str_to_uint16_t(&start_addr, argv[optind + 2])) {
+				fprintf(stderr, "Error: Bad starting address\n");
+				return -1;
+			}
+			if (str_to_uint16_t(&stop_addr, argv[optind + 3])) {
+				fprintf(stderr, "Error: Bad ending address\n");
+				return -1;
+			}
+			start_given = true;
+			stop_given = true;
+			break;
+
+		/* Too many positional arguments given */
+		default:
+			fprintf(stderr, "Error: Too many positional arguments\n");
+			print_usage();
+			return 1;
+	}
+	result = bram_create(&bram, uio_number, map_number);
+	if (result) {
+		fprintf(stderr, "Could not create block RAM resource for UIO device %d "
+				"or map number %d\n", uio_number, map_number);
+		return 1;
+	}
+
+	/* Need start and stop addresses before purge can begin */
+	if (!stop_given) {
+		stop_addr = bram.map_size - 1;
+	}
+	if (!start_given) {
+		start_addr = 0;
+	}
+
+	/* If bounds check before purging fails, we still have to exit cleanly */
+	if (start_addr > stop_addr) {
+		fprintf(stderr, "Error: Start address is greater than stop address\n");
+		retval = 1;
+		goto err_exit;
+	}
+	if (stop_addr > (bram.map_size - 1)) {
+		fprintf(stderr, "Error: Stop address cannot exceed map size\n");
+		retval = 1;
+		goto err_exit;
+	}
+	/*
+	 * It's too annoying to try to manage the case where multiple purge
+	 * patterns are selected, so just select them in the order they're
+	 * checked for here. If the user hasn't selected one, then we just fall
+	 * out the bottom with nothing to do.
+	 */
+	if (by_value) {
+		retval = purge_bram_by_value(&bram, start_addr, stop_addr, purge_val);
+	} else if (by_xboard) {
+		retval = purge_bram_by_xboard(&bram, start_addr, stop_addr);
+	} else if (by_incr) {
+		retval = purge_bram_by_incr(&bram, start_addr, stop_addr);
 	} else {
-		uio_number = atoi(argv[optind]);
-		map_number = atoi(argv[optind + 1]);
-		if (str_to_uint8_t(&purge_val, argv[optind + 2])) {
-			fprintf(stderr, "Error Bad purge value\n");
-			return -1;
-		}
-		/* This should never be able to occur */
-		assert(argc >= (optind - 3));
-		printf("argc = %d\n", argc);
-		printf("optind = %d\n", optind);
-		/* Still need to validate addresses against the actual block RAM */
-		switch (argc - optind - 3) {
-			/* No start or end address given - extract later */
-			case 0:
-				start_given = false;
-				end_given = false;
-				break;
-			/* Only given starting address - extract end later */
-			case 1:
-				if (str_to_uint16_t(&start_addr, argv[optind + 3])) {
-					fprintf(stderr, "Error: Bad starting address\n");
-					return -1;
-				}
-				start_given = true;
-				end_given = false;
-				break;
-			/* Start and end addresses given */
-			case 2:
-				if (str_to_uint16_t(&start_addr, argv[optind + 3])) {
-					fprintf(stderr, "Error: Bad starting address\n");
-					return -1;
-				}
-				if (str_to_uint16_t(&end_addr, argv[optind + 4])) {
-					fprintf(stderr, "Error: Bad ending address\n");
-					return -1;
-				}
-				start_given = true;
-				end_given = true;
-				break;
-
-			/* Too many positional arguments given */
-			default:
-				fprintf(stderr, "Error: Too many positional arguments\n");
-				print_usage();
-				return 1;
-		}
+		printf("No purge pattern selected. Exiting\n");
+		retval = 1;
 	}
 
-	printf("uio = %d\n", uio_number);
-	printf("map = %d\n", map_number);
-	printf("purge = 0x%02"PRIx8"\n", purge_val);
-	if (start_given) {
-		printf("start = 0x%04"PRIx16"\n", start_addr);
+err_exit:
+	if (bram_destroy(&bram)) {
+		fprintf(stderr, "Could not destroy block RAM resource\n");
+		retval = 1;
 	}
-	if (end_given) {
-		printf("end = 0x%04"PRIx16"\n", end_addr);
-	}
+	/* Either we failed a bounds check or purging was unsuccessful */
+	return retval;
 
 }
 
