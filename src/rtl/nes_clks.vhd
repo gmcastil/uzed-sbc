4a69da12bd847e3f5457ee7f50b991d20fd153fc
George Castillo <gmcastil@gmail.com>
2023-11-18 08:05:37 -0700

    Adding clock and reset module for NES emulator

--- src/rtl/nes_clks.vhd
+++ src/rtl/nes_clks.vhd
@@ -5,39 +5,114 @@ library UNISIM;
 use UNISIM.vcomponents.all;
 
 entity nes_clks is
+    generic (
+        -- If desired, an IBUF can be inserted on either the input clock
+        -- or external reset signals. No BUFG is required or should be included
+        -- on the input clock, unless external logic needs to run on that
+        -- domain.
+        CLK_IN_IBUF             : boolean   := false;
+        RST_IN_IBUF             : boolean   := false;
+        -- Number of `clk_ref` ticks before `rst_ref` is deasserted
+        REF_RST_LENGTH          : natural   := 10;
+        -- Number of `clk_mst` ticks before `rst_ref` is deasserted
+        MST_RST_LENGTH          : natural   := 10;
+        -- Number of `clk_en_ppu` ticks before `rst_en_ppu` is deasserted
+        PPU_EN_RST_LENGTH       : natural   := 4;
+        -- Number of `clk_en_cpu` ticks before `rst_en_cpu` is deasserted
+        CPU_EN_RST_LENGTH       : natural   := 4
+    );
     port (
-
         -- Nominally a 100MHz input clock, but other values may be possible,
-        -- if the MMCM is reconfigured.  Internally, we generate a 236.25MHz
-        -- clock and then create the CPU and PPU clocks
-        clk_ext         :   in      std_logic;
+        -- with modifications to the MMCM configuration.  Internally, we
+        -- first generate a 236.25MHz clock, divide by 11 to get the 21.477MHz
+        -- master clock and the CPU and PPU clock enables from this.
+        clk_ext         : in    std_logic;
+        -- External active-high reset tied directly to the MMCM reset input.
+        rst_ext         : in    std_logic;
 
-        -- CPU and PPU clocks
-        clk_cpu         :   out     std_logic;
-        clk_ppu         :   out     std_logic
+        -- 236.25MHz clock synthesized from 100MHz input
+        clk_ref         : out   std_logic;
+        -- 21.477MHz master clock divided by 11 from the 236.25MHz clock
+        clk_mst         : out   std_logic;
+        -- PPU clock enable created by dividing the master clock by 5
+        clk_en_ppu      : out   std_logic;
+        -- CPU clock enable created by dividing the master clock by 12
+        clk_en_cpu      : out   std_logic;
+        
+        -- Active low reset synchronous to 236.25MHz clock
+        rst_ref         : out   std_logic;
+        -- Active low reset synchronous to 21.477MHz master clock. 
+        rst_mst         : out   std_logic;
+        -- Active low resets synchronous to the 21.477MHz master clock, but
+        -- timed to deassert coincident with the rising edge of the PPU and
+        -- CPU clock enables. It is recommended that circuits that use the
+        -- divided clock enables are reset with these signals.
+        rst_en_ppu      : out   std_logic;
+        rst_en_cpu      : out   std_logic
     );
 end nes_clks;
 
 architecture structural of nes_clks is
 
+    signal  clk_mmcm        : std_logic;
+    signal  rst_mmcm        : std_logic;
+
+    signal  clk_236m25      : std_logic;
+    signal  clk_21m477      : std_logic;
+    signal  clk_fb          : std_logic;
+    signal  mmcm_locked     : std_logic;
+
 begin
 
-    MMCME2_ADV_inst : MMCME2_ADV
+    -- If indicated, add IBUF to the input clock or reset
+    clk_buffer : if CLK_IN_IBUF generate
+    begin
+        IBUF_clk_ext : IBUF
+        generic map (
+            IBUF_LOW_PWR    => TRUE,
+            IOSTANDARD      => "DEFAULT"
+        )
+        port map (
+            O               => clk_mmcm,
+            I               => clk_ext
+        );
+    else generate
+        clk_mmcm            => clk_ext;
+    end generate
+
+    rst_buffer : if RST_IN_IBUF generate
+    begin
+        IBUF_rst_ext : IBUF
+        generic map (
+            IBUF_LOW_PWR    => TRUE,
+            IOSTANDARD      => "DEFAULT"
+        )
+        port map (
+            O               => rst_mmcm;
+            I               => rst_ext
+        );
+    else generate
+        rst_mmcm            => rst_ext;
+    end generate
+
+    -- Synthesize the 236.25 and 21.477 MHz internal clocks from the 100MHz
+    -- input clock
+    MMCME2_ADV_i0 : MMCME2_ADV
     generic map (
         BANDWIDTH               => "OPTIMIZED",
-        CLKFBOUT_MULT_F         => 5.0,
+        CLKFBOUT_MULT_F         => 47.250,
         CLKFBOUT_PHASE          => 0.0,
         -- CLKIN_PERIOD: Input clock period in ns to ps resolution (i.e. 33.333 is 30 MHz).
-        CLKIN1_PERIOD           => 0.0,
+        CLKIN1_PERIOD           => 10.0,
         CLKIN2_PERIOD           => 0.0,
         -- CLKOUT0_DIVIDE - CLKOUT6_DIVIDE: Divide amount for CLKOUT (1-128)
-        CLKOUT1_DIVIDE          => 1,
+        CLKOUT1_DIVIDE          => 44,
         CLKOUT2_DIVIDE          => 1,
         CLKOUT3_DIVIDE          => 1,
         CLKOUT4_DIVIDE          => 1,
         CLKOUT5_DIVIDE          => 1,
         CLKOUT6_DIVIDE          => 1,
-        CLKOUT0_DIVIDE_F        => 1.0,
+        CLKOUT0_DIVIDE_F        => 4.0,
         -- CLKOUT0_DUTY_CYCLE - CLKOUT6_DUTY_CYCLE: Duty cycle for CLKOUT outputs (0.01-0.99).
         CLKOUT0_DUTY_CYCLE      => 0.5,
         CLKOUT1_DUTY_CYCLE      => 0.5,
@@ -56,7 +131,7 @@ begin
         CLKOUT6_PHASE           => 0.0,
         CLKOUT4_CASCADE         => FALSE,
         COMPENSATION            => "ZHOLD",
-        DIVCLK_DIVIDE           => 1,
+        DIVCLK_DIVIDE           => 5,
         -- REF_JITTER: Reference input jitter in UI (0.000-0.999).
         REF_JITTER1             => 0.0,
         REF_JITTER2             => 0.0,
@@ -74,106 +149,54 @@ begin
         CLKOUT4_USE_FINE_PS     => FALSE,
         CLKOUT5_USE_FINE_PS     => FALSE,
         CLKOUT6_USE_FINE_PS     => FALSE
+    )
+    port map (
+        -- Clock Outputs: 1-bit (each) output: User configurable clock outputs
+        CLKOUT0                 => clk_236m25,
+        CLKOUT0B                => open,
+        CLKOUT1                 => clk_21m477,
+        CLKOUT1B                => open,
+        CLKOUT2                 => open,
+        CLKOUT2B                => open,
+        CLKOUT3                 => open,
+        CLKOUT3B                => open,
+        CLKOUT4                 => open,
+        CLKOUT5                 => open,
+        CLKOUT6                 => open,
+        -- DRP Ports: 16-bit (each) output: Dynamic reconfiguration ports
+        DO                      => open,
+        DRDY                    => open,
+        -- Dynamic Phase Shift Ports: 1-bit (each) output: Ports used for dynamic phase shifting of the outputs
+        PSDONE                  => open,
+        -- Feedback Clocks: 1-bit (each) output: Clock feedback ports
+        CLKFBOUT                => clk_fb,
+        CLKFBOUTB               => open,
+        -- Status Ports: 1-bit (each) output: MMCM status ports
+        CLKFBSTOPPED            => open,
+        CLKINSTOPPED            => open,
+        LOCKED                  => mmcm_locked,
+        -- Clock Inputs: 1-bit (each) input: Clock inputs
+        CLKIN1                  => clk_mmcm,
+        CLKIN2                  => '0',
+        -- Control Ports: 1-bit (each) input: MMCM control ports
+        CLKINSEL                => '1',
+        PWRDWN                  => '0',
+        RST                     => rst_mmcm,
+        -- DRP Ports: 7-bit (each) input: Dynamic reconfiguration ports
+        DADDR                   => (others=>'0'),
+        DCLK                    => '0',
+        DEN                     => '0',
+        DI                      => (others=>'0'),
+        DWE                     => '0',
+        -- Dynamic Phase Shift Ports: 1-bit (each) input: Ports used for dynamic phase shifting of the outputs
+        PSCLK                   => '0',
+        PSEN                    => '0',
+        PSINCDEC                => '0',
+        -- Feedback Clocks: 1-bit (each) input: Clock feedback ports
+        CLKFBIN                 => clk_fb
+        );
 
-    MMCME2_ADV_inst : MMCME2_ADV
-   generic map (
-      BANDWIDTH => "OPTIMIZED",      -- Jitter programming (OPTIMIZED, HIGH, LOW)
-      CLKFBOUT_MULT_F => 5.0,        -- Multiply value for all CLKOUT (2.000-64.000).
-      CLKFBOUT_PHASE => 0.0,         -- Phase offset in degrees of CLKFB (-360.000-360.000).
-      -- CLKIN_PERIOD: Input clock period in ns to ps resolution (i.e. 33.333 is 30 MHz).
-      CLKIN1_PERIOD => 0.0,
-      CLKIN2_PERIOD => 0.0,
-      -- CLKOUT0_DIVIDE - CLKOUT6_DIVIDE: Divide amount for CLKOUT (1-128)
-      CLKOUT1_DIVIDE => 1,
-      CLKOUT2_DIVIDE => 1,
-      CLKOUT3_DIVIDE => 1,
-      CLKOUT4_DIVIDE => 1,
-      CLKOUT5_DIVIDE => 1,
-      CLKOUT6_DIVIDE => 1,
-      CLKOUT0_DIVIDE_F => 1.0,       -- Divide amount for CLKOUT0 (1.000-128.000).
-      -- CLKOUT0_DUTY_CYCLE - CLKOUT6_DUTY_CYCLE: Duty cycle for CLKOUT outputs (0.01-0.99).
-      CLKOUT0_DUTY_CYCLE => 0.5,
-      CLKOUT1_DUTY_CYCLE => 0.5,
-      CLKOUT2_DUTY_CYCLE => 0.5,
-      CLKOUT3_DUTY_CYCLE => 0.5,
-      CLKOUT4_DUTY_CYCLE => 0.5,
-      CLKOUT5_DUTY_CYCLE => 0.5,
-      CLKOUT6_DUTY_CYCLE => 0.5,
-      -- CLKOUT0_PHASE - CLKOUT6_PHASE: Phase offset for CLKOUT outputs (-360.000-360.000).
-      CLKOUT0_PHASE => 0.0,
-      CLKOUT1_PHASE => 0.0,
-      CLKOUT2_PHASE => 0.0,
-      CLKOUT3_PHASE => 0.0,
-      CLKOUT4_PHASE => 0.0,
-      CLKOUT5_PHASE => 0.0,
-      CLKOUT6_PHASE => 0.0,
-      CLKOUT4_CASCADE => FALSE,      -- Cascade CLKOUT4 counter with CLKOUT6 (FALSE, TRUE)
-      COMPENSATION => "ZHOLD",       -- ZHOLD, BUF_IN, EXTERNAL, INTERNAL
-      DIVCLK_DIVIDE => 1,            -- Master division value (1-106)
-      -- REF_JITTER: Reference input jitter in UI (0.000-0.999).
-      REF_JITTER1 => 0.0,
-      REF_JITTER2 => 0.0,
-      STARTUP_WAIT => FALSE,         -- Delays DONE until MMCM is locked (FALSE, TRUE)
-      -- Spread Spectrum: Spread Spectrum Attributes
-      SS_EN => "FALSE",              -- Enables spread spectrum (FALSE, TRUE)
-      SS_MODE => "CENTER_HIGH",      -- CENTER_HIGH, CENTER_LOW, DOWN_HIGH, DOWN_LOW
-      SS_MOD_PERIOD => 10000,        -- Spread spectrum modulation period (ns) (VALUES)
-      -- USE_FINE_PS: Fine phase shift enable (TRUE/FALSE)
-      CLKFBOUT_USE_FINE_PS => FALSE,
-      CLKOUT0_USE_FINE_PS => FALSE,
-      CLKOUT1_USE_FINE_PS => FALSE,
-      CLKOUT2_USE_FINE_PS => FALSE,
-      CLKOUT3_USE_FINE_PS => FALSE,
-      CLKOUT4_USE_FINE_PS => FALSE,
-      CLKOUT5_USE_FINE_PS => FALSE,
-      CLKOUT6_USE_FINE_PS => FALSE
-   )
-   port map (
-      -- Clock Outputs: 1-bit (each) output: User configurable clock outputs
-      CLKOUT0 => CLKOUT0,           -- 1-bit output: CLKOUT0
-      CLKOUT0B => CLKOUT0B,         -- 1-bit output: Inverted CLKOUT0
-      CLKOUT1 => CLKOUT1,           -- 1-bit output: CLKOUT1
-      CLKOUT1B => CLKOUT1B,         -- 1-bit output: Inverted CLKOUT1
-      CLKOUT2 => CLKOUT2,           -- 1-bit output: CLKOUT2
-      CLKOUT2B => CLKOUT2B,         -- 1-bit output: Inverted CLKOUT2
-      CLKOUT3 => CLKOUT3,           -- 1-bit output: CLKOUT3
-      CLKOUT3B => CLKOUT3B,         -- 1-bit output: Inverted CLKOUT3
-      CLKOUT4 => CLKOUT4,           -- 1-bit output: CLKOUT4
-      CLKOUT5 => CLKOUT5,           -- 1-bit output: CLKOUT5
-      CLKOUT6 => CLKOUT6,           -- 1-bit output: CLKOUT6
-      -- DRP Ports: 16-bit (each) output: Dynamic reconfiguration ports
-      DO => DO,                     -- 16-bit output: DRP data
-      DRDY => DRDY,                 -- 1-bit output: DRP ready
-      -- Dynamic Phase Shift Ports: 1-bit (each) output: Ports used for dynamic phase shifting of the outputs
-      PSDONE => PSDONE,             -- 1-bit output: Phase shift done
-      -- Feedback Clocks: 1-bit (each) output: Clock feedback ports
-      CLKFBOUT => CLKFBOUT,         -- 1-bit output: Feedback clock
-      CLKFBOUTB => CLKFBOUTB,       -- 1-bit output: Inverted CLKFBOUT
-      -- Status Ports: 1-bit (each) output: MMCM status ports
-      CLKFBSTOPPED => CLKFBSTOPPED, -- 1-bit output: Feedback clock stopped
-      CLKINSTOPPED => CLKINSTOPPED, -- 1-bit output: Input clock stopped
-      LOCKED => LOCKED,             -- 1-bit output: LOCK
-      -- Clock Inputs: 1-bit (each) input: Clock inputs
-      CLKIN1 => CLKIN1,             -- 1-bit input: Primary clock
-      CLKIN2 => CLKIN2,             -- 1-bit input: Secondary clock
-      -- Control Ports: 1-bit (each) input: MMCM control ports
-      CLKINSEL => CLKINSEL,         -- 1-bit input: Clock select, High=CLKIN1 Low=CLKIN2
-      PWRDWN => PWRDWN,             -- 1-bit input: Power-down
-      RST => RST,                   -- 1-bit input: Reset
-      -- DRP Ports: 7-bit (each) input: Dynamic reconfiguration ports
-      DADDR => DADDR,               -- 7-bit input: DRP address
-      DCLK => DCLK,                 -- 1-bit input: DRP clock
-      DEN => DEN,                   -- 1-bit input: DRP enable
-      DI => DI,                     -- 16-bit input: DRP data
-      DWE => DWE,                   -- 1-bit input: DRP write enable
-      -- Dynamic Phase Shift Ports: 1-bit (each) input: Ports used for dynamic phase shifting of the outputs
-      PSCLK => PSCLK,               -- 1-bit input: Phase shift clock
-      PSEN => PSEN,                 -- 1-bit input: Phase shift enable
-      PSINCDEC => PSINCDEC,         -- 1-bit input: Phase shift increment/decrement
-      -- Feedback Clocks: 1-bit (each) input: Clock feedback ports
-      CLKFBIN => CLKFBIN            -- 1-bit input: Feedback clock
-   );
-
+        -- Add the two synthesized clocks to the global clock network
 
 end structural;
 
