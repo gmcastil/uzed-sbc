2caf039e4606dc7a852b921cbf3117460ef7220d
George Castillo <gmcastil@gmail.com>
2023-11-19 10:02:52 -0700

    First cut at the EEPROM emulator is done.
    - I haven't added the XCI file for the block RAM yet
    - And it also occurrs to me that at some point I'm going to need to
    start version controlling the project I've been working with on the side
    as well as the IP that is used to make it
    - I hate dealing with projects and revision control but I have spent a
    tremendous amount of time trying to find ways to avoid using projects
    and the IPI flow that Xilinx recommends and I'm not happy with anything
    that I've come up with.
    - I may just make a project, strip out all of the stuff that isn't
    necessary, and then check that bare project in.

--- src/rtl/at28c256.vhd
+++ src/rtl/at28c256.vhd
@@ -7,23 +7,76 @@
 -- one interface, but it can be programmed, written to, purged, etc. from a higher level of abstraction
 -- such as software running on a processor via a block RAM controller. 
 
+library ieee;
+use ieee.std_logic_1164.all;
+
 entity at28c256
     port (
+        -- PS memory controller interface
+        ps_clk			: in    std_logic;
+        ps_en			: in    std_logic;
+        ps_we			: in    std_logic;
+        ps_addr		    : in    std_logic_vector(14 downto 0);
+        ps_din			: in    std_logic_vector(7 downto 0);
+        ps_dout			: in    std_logic_vector(7 downto 0);
 
-        -- PS BRAM controller interface
-        -- This section needs to connect up to a Xilinx block RAM controller
-
-        -- PL SRAM interface
-        clk         :   in      std_logic;
-        chip_enb    :   in      std_logic;
-        output_enb  :   in      std_logic;
-        wr_enb      :   in      std_logic;
-        addr        :   in      std_logic_vector(14 downto 0);
-        rd_data     :   out     std_logic_vector(7 downto 0);
-        wr_data     :   in      std_logic_vector(7 downto 0)
+        -- The SBC processor sees this as an asynchronous memory, so we supply a clock that is
+        -- synchronous to the SBC memory interface signals, but sufficiently fast such that
+        -- accesses are completed in the expected interval.
+        sbc_clk         : in    std_logic;
+        -- Depending upon how the address decoder logic of the processor is structured, all or less
+        -- of the EEPROM emulator can be used. A common application might be to only connect up the
+        -- first 14 bits of the EEPROM to get 16KB of ROM.
+        sbc_addr        : in    std_logic_vector(14 downto 0);
+        -- The control signals are not simply wired to block RAM.
+        sbc_ceb         : in    std_logic;
+        sbc_oeb         : in    std_logic;
+        -- Writes to the EEPROM are not supported, but this signal does need to be driven
+        -- appropriately.
+        sbc_web         : in    std_logic;
+        sbc_rd_data     : out   std_logic_vector(7 downto 0);
     );
 
+end entity at28c256;
 
+architecture struct of at28c256 is
 
+    signal doutb    : std_logic_vector(7 downto 0);
 
+begin
 
+    -- Using these signals in this manner incurs an extra cycle of latency to accesses from
+    -- fabric logic (access from the PS is unaffected). An alternate choice here would be to 
+    -- make this unclocked and incur some additional routing delay. The actual EEPROM puts the
+    -- outputs in a high impedance state because in a real application, the data bus is shared.
+    sbc_read: process(sbc_clk) is
+    begin
+        if sbc_web and not sbc_ceb and not sbc_oeb then
+            sbc_rd_data     <= doutb;
+        else
+            sbc_rd_data     <= sbc_rd_data;
+        end if;
+
+    end process sbc_read;
+
+    -- True dual port RAM with independent clocks, three cycles of read latency and configured for a
+    -- depth of 32K and width of 8-bits. The PS side is brought up to the top so that it can be
+    -- connected directly to the PS. Note that since we are emulating a ROM, we disable the write
+    -- feature from the SBC side to prevent inadvertent data corruption.
+    trp_bram_32kx8_i0: tdp_bram_32kx8
+    port map (
+        clka	=> ps_clk,
+        ena		=> ps_en,
+        wea		=> ps_we,
+        addra	=> ps_addr,
+        dina	=> ps_din,
+        douta	=> ps_dout,
+        clkb	=> sbc_clk,
+        enb		=> '1',
+        web		=> '0',
+        addrb	=> sbc_addrb,
+        dinb	=> open,
+        doutb	=> doutb
+    );
+
+end architecture struct;
