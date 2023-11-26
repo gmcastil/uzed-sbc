ec7984dbefb0a365d4d86297e4284383cbbfe89c
George Castillo <gmcastil@gmail.com>
2023-11-26 16:23:07 -0700

    Started working on adding a VIO / ILA combination to the ROM emulator
    - It isn't clear to me the way addressing and byte access is done
    at the two interfaces of the block RAM (at least, now that the actual
    processor SBC block design is being implemented in hardware)
    - The simplest way is to just add a VIO that can poke the memory in
    hardware and dump it out to chipscope.
    - I'm not trying to add a third port or a backdoor to the memory; that
    already exists via the PS. I just need to verify that bytes go in and
    out of memory the way that I think they do and that hasn't been shown to
    my satisfaction yet.
    - Once I've verified the hardware behavior, we can look at how to modify
    the block RAM support software.

--- src/rtl/at28c256.vhd
+++ src/rtl/at28c256.vhd
@@ -18,11 +18,18 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 library UNISIM;
 use UNISIM.vcomponents.all;
 
 entity at28c256 is
+    generic (
+        -- When true, replace the PL side memory interface with an ILA and VIO that allows
+        -- manual dumping of memory pages (possibly useful for debugging PS side AXI access)
+        DEBUG_PAGE_DUMP_ENABLED     : boolean   := false;
+        BRAM_RD_LATENCY             : integer   := 3
+    )
     port (
         -- PS memory controller interface
         ps_clk          : in    std_logic;
@@ -53,19 +60,56 @@ end entity at28c256;
 
 architecture structural of at28c256 is
 
-    signal douta    : std_logic_vector(7 downto 0);
+    signal douta            : std_logic_vector(7 downto 0);
+
+    -- We wish to add the ability to dump a 4KB page of the block RAM from the hardware side to
+    -- chipscope if a VIO and ILA are enabled during synthesis.
+    signal dump_start       : std_logic;
+    signal dump_page        : unsigned(2 downto 0);
+    signal dump_cnt         : unsigned(12 downto 0);
+    signal dump_addr        : std_logic(14 downto 0);
+    signal dump_rd_stall    : unsigned(2 downto 0);
+
+    component tdp_bram_32kx8
+    port (
+        clka                : in    std_logic;
+        ena                 : in    std_logic;
+        wea                 : in    std_logic_vector(0 downto 0);
+        addra               : in    std_logic_vector(14 downto 0);
+        dina                : in    std_logic_vector(7 downto 0);
+        douta               : out   std_logic_vector(7 downto 0);
+        clkb                : in    std_logic;
+        enb                 : in    std_logic;
+        web                 : in    std_logic_vector(3 downto 0);
+        addrb               : in    std_logic_vector(12 downto 0);
+        dinb                : in    std_logic_vector(31 downto 0);
+        doutb               : out   std_logic_vector(31 downto 0)
+    );
+    end component;
+
+    component vio_dump_page_ctrl
+    port (
+        clk                 : in    std_logic;
+        probe_out0          : out   std_logic_vector(0 downto 0);
+        probe_out1          : out   std_logic_vector(2 downto 0)
+    );
+    end component;
+
 
 begin
 
-    -- Using these signals in this manner incurs an extra cycle of latency to accesses from
-    -- fabric logic (access from the PS is unaffected). An alternate choice here would be to
-    -- make this unclocked and incur some additional routing delay. The actual EEPROM puts the
-    -- outputs in a high impedance state because in a real application, the data bus is shared.
+    -- This logic is straight out of the AT28C256 datasheet, but in our case with registered outputs
+    -- and an extra cycle of latency. It is expected that the processor is only looking at the ROM
+    -- during clock cycles when the CPU clock enable is asserted, which is /12 the SBC clock rate.
+    -- To make it easier to detect problems, we drive the data bus with all ones unless it is
+    -- enabled properly (even though we have the data from the memory).
     sbc_read: process (sbc_clk) is
     begin
         if rising_edge(sbc_clk) then
             if (sbc_web = '1') and (sbc_ceb = '0') and (sbc_oeb = '0') then
-                sbc_rd_data     <= douta;
+                sbc_rd_data     <= pl_rd_data;
+            else
+                sbc_rd_data     <= (others=>'1');
             end if;
         end if;
     end process sbc_read;
@@ -91,11 +135,11 @@ begin
     trp_bram_32kx8_i0: entity work.tdp_bram_32kx8
     port map (
         clka    => sbc_clk,
-        ena     => '1',
+        ena     => pl_en,
         wea     => (others=>'0'),
-        addra   => sbc_addr,
+        addra   => pl_addr,
         dina    => (others=>'0'),
-        douta   => douta,
+        douta   => pl_rd_data
         clkb    => ps_clk,
         enb     => ps_en,
         web     => ps_we,
@@ -103,5 +147,74 @@ begin
         dinb    => ps_wr_data,
         doutb   => ps_rd_data
     );
+
+    if DEBUG_PAGE_DUMP_ENABLED generate
+        -- Instantiate a VIO
+        dump_page_ctrl: vio_dump_page_ctrl
+        port map (
+            clk i       => sbc_clk,
+            probe_out0  => dump_start,
+            probe_out1  => dump_page
+        );
+
+        -- Need a rising edge detector to actually initiaiate the block RAM dump
+        dump_start_red: process(sbc_clk)
+        begin
+            if rising_edge(sbc_clk) then
+                dump_start_q        <= dump_start;
+                dump_start_qq       <= dump_start_q;
+                if (dump_start_qq = '0' and dump_start_q = '1') then
+                    dump_start_red  <= '1';
+                else
+                    dump_start_red  <= '0';
+                end if;
+            end if;
+        end process dump_start_red;
+
+        -- The dump address is always just the concatenation of the page we're dumping
+        -- and the counter value that we're incrementing
+        dump_addr           <= std_logic_vector(dump_page) & std_logic_vector(dump_cnt);
+
+        -- Now we need to increment the counter after the start indicator is given and then
+        -- wait the appropriate number of clocks to let the BRAM service the read request
+        dump_page: process(sbc_clk)
+        begin
+            if rising_edge(clk) then
+                if (dump_start_red = '1' and dump_busy = '0') then
+                    dump_busy       <= '1';
+                    dump_rd_stall   <= 0;
+                elsif (dump_busy = '1') then
+                    -- Data should have arrived
+                    if (dump_rd_stall != BRAM_RD_LATENCY) then
+
+
+                    -- Still need to wait for BRAM to service request
+                        else
+
+                else
+
+                end if;
+            end if;
+        end process dump_page;
+
+        -- Instantiate an ILA
+        dump_page_mon: ila_dump_page_mon 
+        port map (
+            clk         => sbc_clk,
+            probe0      => dump_start_red,
+            probe1      => dump_addr,
+            probe2      => dump_busy,
+            probe3      => dump_rd_stall,
+            probe3      => pl_rd_data
+        );
+
+
+    else generate
+        -- When we're not debugging this interface, we just wire the enable
+        -- high all the time and the address at the BRAM is the same as the address
+        -- from the SBC.
+        pl_addr     <= sbc_addr;
+        pl_en       <= '1';
+    end generate
 
 end architecture structural;
